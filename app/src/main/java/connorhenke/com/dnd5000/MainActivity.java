092fdbcd996e61f17b46a540e2596a85a3437802
Tony DiPerna <chilidog069@gmail.com>
2016-10-06 21:23:19 -0500

    remove unused import

--- app/src/main/java/connorhenke/com/dnd5000/MainActivity.java
+++ app/src/main/java/connorhenke/com/dnd5000/MainActivity.java
@@ -25,7 +25,6 @@ import android.view.MenuItem;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
